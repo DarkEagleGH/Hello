910bd0f4cde2e04d2e47ca0124107c5e196994d1
darkeagle_bb <darkeagle.mail@gmail.com>
2016-11-23 23:56:29 +0200

    clean

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,5 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Locale;
 
 public class Hello {
     private static final Logger logger = LoggerFactory.getLogger(Hello.class);
